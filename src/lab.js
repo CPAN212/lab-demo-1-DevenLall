e21b5220961c8c5e0e991b772c28d9ff44b41fea
DevenLall <devenlall05@gmail.com>
2025-09-04 20:56:08 -0400

    completed lab demo task

--- src/lab.js
+++ src/lab.js
@@ -3,7 +3,12 @@
 // Input: [1, 2, 3, 4]
 // Output: [4, 3, 2, 1]
 function reverseArray(arr) {
-  // Write you solution here
+  const num = [1, 2, 3, 4];
+  const reverse = [];
+  for(let i = num.length -1; i >= 0; i--){
+    reverse.push(num[i])
+  }
+  return console.log(reverse);
 }
 
 // Write a function that removes duplicate numbers from an array and returns a new array with only unique values.
@@ -11,7 +16,9 @@ function reverseArray(arr) {
 // Input: [1, 2, 2, 3, 4, 4, 5]
 // Output: [1, 2, 3, 4, 5]
 function removeDuplicates(arr) {
-  // Write you solution here
+  const array1 = [1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5]
+  const array2 = [...new Set(array1)]
+  console.log(array2)
 }
 
 // Write a function that sorts an array of numbers in ascending order (smallest to largest).
@@ -19,7 +26,9 @@ function removeDuplicates(arr) {
 // Input: [5, 2, 9, 1, 5, 6]
 // Output: [1, 2, 5, 5, 6, 9]
 function sortArray(arr) {
-  // Write you solution here
+  const num = [4, 2, 7, 6, 8];
+  num.sort()
+  console.log(num)
 }
 
 // You have a synchronous function that returns a greeting message with your name.
@@ -29,8 +38,16 @@ function syncGreet(name) {
 }
 
 function asyncGreet(name) {
-  // Write you solution here
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const message = syncGreet(name);
+      console.log("resolving", message);
+      resolve(message);
+    }, 2000);
+    console.log("test")
+  });
 }
+
 
 module.exports = {
   reverseArray,
@@ -38,3 +55,5 @@ module.exports = {
   sortArray,
   asyncGreet,
 };
+
+reverseArray()
