90698779928e83cad1a955042409d9b4cd9569d2
DevenLall <devenlall05@gmail.com>
2025-09-04 21:25:16 -0400

    completed demo lab task

--- src/lab.js
+++ src/lab.js
@@ -4,17 +4,15 @@
 // Output: [4, 3, 2, 1]
 function reverseArray(arr) {
   arr.reverse();
-  console.log(arr)
+  return arr
 }
-
 // Write a function that removes duplicate numbers from an array and returns a new array with only unique values.
 // Example:
 // Input: [1, 2, 2, 3, 4, 4, 5]
 // Output: [1, 2, 3, 4, 5]
 function removeDuplicates(arr) {
-  const array1 = [1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5]
-  const array2 = [...new Set(array1)]
-  console.log(array2)
+  const array2 = [...new Set(arr)]
+  return array2
 }
 
 // Write a function that sorts an array of numbers in ascending order (smallest to largest).
@@ -22,9 +20,8 @@ function removeDuplicates(arr) {
 // Input: [5, 2, 9, 1, 5, 6]
 // Output: [1, 2, 5, 5, 6, 9]
 function sortArray(arr) {
-  const num = [4, 2, 7, 6, 8];
-  num.sort()
-  console.log(num)
+  arr.sort()
+  return arr
 }
 
 // You have a synchronous function that returns a greeting message with your name.
